6401a768d3cd6d024745eea6c76967799bb08ae5
rodrigobn <rodrito90@gmail.com>
2020-11-17 21:19:22 -0300

    Iniciando projeto

---
+++ app/src/main/java/br/com/rodrigobrito/filmescoroutines/MainActivity.kt
@@ -0,0 +1,18 @@
+package br.com.rodrigobrito.filmescoroutines
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import br.com.rodrigobrito.filmescoroutines.ui.main.MainFragment
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.main_activity)
+        if (savedInstanceState == null) {
+            supportFragmentManager.beginTransaction()
+                    .replace(R.id.container, MainFragment.newInstance())
+                    .commitNow()
+        }
+    }
+}
